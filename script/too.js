ee14931aef4b7eb89b23ca2b46d8f4743ef45121
Lusi <l.ryborz@sintlucasedu.nl>
2022-11-01 13:44:08 +0100

    update 1

---
+++ script/too.js
@@ -0,0 +1,14 @@
+function copydisc() 
+{
+    var copyText = "Rattollinie#3583";
+    navigator.clipboard.writeText(copyText);
+    alert("Copied the text: " + copyText);
+  }
+
+
+  function copynumb()
+   {
+    var copyText = "013 06 27188347";
+    navigator.clipboard.writeText(copyText);
+    alert("Copied the text: " + copyText);
+  }
